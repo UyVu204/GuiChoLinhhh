26041aa218499d79e477c5c723b80852ecad9a1a
UyVu204 <85274337+UyVu204@users.noreply.github.com>
2021-06-03 13:24:28 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
 const CONFIG = {
-    introTitle: 'Linh à!',
+    introTitle: 'Thi yang hồ à!',
     introDesc: `Trái đất vốn lạ thường
     Sao t cứ đi nhầm đường vậy taa
-    đoạn đường dù gần hay xa chỉ cần em cười 1 xíu
+    đoạn đường dù gần hay xa chỉ cần m cười 1 xíu
     là vơi đi nỗi buồn :33 `,
     btnIntro: 'hihi',
     title: 'Phải chăng Anhhhh đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng Anhh đã say ngay từ lúc thấy nụ cười ấy :33 ',
-    btnYes: 'Vẫn cứ là thích anh <33',
+    btnYes: 'Thích t chớ gì <33',
     btnNo: 'Không, Cậu à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao m lại chọn t <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    reply: 'Yêu thì yêu mà không yêu thì vẫn phải yêu nghe hăm <33333333',
+    mess: 'tau biết mà . Yêu m nhiều nhiều 😘😘',
     messDesc: 'moaww yêu emm.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
