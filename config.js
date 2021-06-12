b23209a89cb45a8ce677186c628972bffd22106f
UyVu204 <85274337+UyVu204@users.noreply.github.com>
2021-06-12 15:39:24 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Chị Hiền Yêu Ơiii!',
+    introTitle: 'Bạn Linhhh CUte Ơiii!',
     introDesc: `Trái đất vốn lạ thường
-    Sao em cứ đi nhầm đường vậy taa
-    đoạn đường này dù gần hay xa chỉ cần chị cười 1 tý
-    là vơi đi nỗi buồn ngay á nên cười nhiều với em nhớ :33 `,
+    Sao cứ đi nhầm đường vậy taa
+    đoạn đường này dù gần hay xa chỉ cần Linh cười 1 tý
+    là vơi đi nỗi buồn ngay á nên cười nhiều với ta nhớ :33 `,
     btnIntro: 'hihi',
-    title: 'Phải chăng Anhhhh đã yêu Chị ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng Anhh đã say ngay từ lúc thấy nụ cười ấy Của chị á :33 ',
-    btnYes: 'Vào Sổ Hộ Khẩu Của emm Nhóaaaa  <33',
-    btnNo: 'Không, M Bị Điên àa :3',
-    question:'Em biết chị sẽ chọn em mừ hihi Loveee youuuu FV <3',
-    btnReply: 'Gửi cho Chồng tương Laiiiii <3',
+    title: 'Phải chăng Anhhhh đã yêu em ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng Anhh đã say ngay từ lúc thấy nụ cười ấy Của Linh á :33 ',
+    btnYes: 'Vào Sổ Hộ Khẩu Của ta Nhóaaa  <33',
+    btnNo: 'Không, Bị Điên àa :3',
+    question:'Ta biết ngươi sẽ chọn ta mừ hihi Loveee youuuu FV <3',
+    btnReply: 'Gửi cho Ny tương Laiiiii <3',
     reply: 'Thế nên không đồng ý vẫn phải đồng ý hihi',
-    mess: 'em biết mà . Yêu chị nhiều nhiều 😘😘',
-    messDesc: ' Mai Game với emmm nhóa <3 Yêu chị  :3.',
+    mess: 'ta biết mà . Yêu ngươi nhiều nhiều 😘😘',
+    messDesc: ' Nào Game nhóaaa <3 Yêu ngươiiiiii  :3.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
