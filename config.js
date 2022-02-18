0b5e42492902b92427df5a616023a06c93922103
UyVu204 <85274337+UyVu204@users.noreply.github.com>
2022-02-18 19:10:09 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,17 @@
 const CONFIG = {
-    introTitle: 'Bạn Linhhh CUte Ơiii!',
-    introDesc: `Trái đất vốn lạ thường
-    Sao cứ đi nhầm đường vậy taa
-    đoạn đường này dù gần hay xa chỉ cần Linh cười 1 tý
-    là vơi đi nỗi buồn ngay á nên cười nhiều với ta nhớ :33 `,
+    introTitle: 'Bạn Thanh cute nhất thế giới ơi!!',
+    introDesc: `cậu ơi tớ biết là tớ làm phiền cậu nhiều lắm thế nên trước hết là tớ xin lỗi cậu nhá kiểu tớ chỉ muốn thích cậu á mà lỡ hơi hơi nhiều người biếc mất gòi nên 
+    cậu hay bị trọc á nên có lẽ cậu sẽ thấy khó chịu hì cả nay cũng làm cậu không thích luôn ha `,
     btnIntro: 'hihi',
     title: 'Phải chăng Anhhhh đã yêu em ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng Anhh đã say ngay từ lúc thấy nụ cười ấy Của Linh á :33 ',
-    btnYes: 'Vào Sổ Hộ Khẩu Của ta Nhóaaa  <33',
-    btnNo: 'Không, Bị Điên àa :3',
-    question:'Ta biết ngươi sẽ chọn ta mừ hihi Loveee youuuu FV <3',
-    btnReply: 'Gửi cho ... tương Laiiiii <3',
+    desc: 'Phải chăng Anhh đã say ngay từ lúc thấy nụ cười ấy :33 ',
+    btnYes: 'Nên là cho tớ xin lỗi nhá hí  <33',
+    btnNo: ' không nhaaaa ',
+    question:'hí hí nó hơi hay hay nên chắc có thể chọc cho cậu vui một xí hehe <3',
+    btnReply: 'Gửi cho cậu xinh gái nhất nhất đối với tớ hiện tại <3',
     reply: 'Thế nên không đồng ý vẫn phải đồng ý hihi',
-    mess: 'ta biết mà . Yêu ngươi nhiều nhiều 😘😘',
-    messDesc: ' Nào Game nhóaaa <3 Yêu ngươiiiiii  :3.',
-    btnAccept: 'Okiiiii lun <3',
+    mess: ' ròi ròi ',
+    messDesc: ' Tớ mong tớ đủ can đảm để nói chuyện với cậu nhiều hơn nữa trong tương lai :3.',
+    btnAccept: 'Cảm ơn cậu gất nhiều luôn ii cậu là s1 ',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
